56e22885d06f7232aeb99ee59b910d2250447347
Amy Hinds <amyhinds@Amys-MacBook-Air.local>
2019-12-19 14:32:05 -0800

    adds beginning of user model

--- tech_scavvy/main_app/models.py
+++ tech_scavvy/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  name = models.CharField(max_length=100)
+  #user = models.ForeignKey(User, on_delete=models.CASCADE)
+  team = models.IntegerField()
+
+  def __str__(self):
+    return f"{self.name} on team {self.team}"
