6b1684ea7ffc5f99646cfe77b0a155f2a1d655e8
Amy Hinds <amyhinds@Amys-MacBook-Air.local>
2019-12-19 14:51:47 -0800

    updates player model and includes foreign keys of django's user and team fkey

--- tech_scavvy/main_app/models.py
+++ tech_scavvy/main_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
+class Player(models.Model):
   name = models.CharField(max_length=100)
-  #user = models.ForeignKey(User, on_delete=models.CASCADE)
-  team = models.IntegerField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
   def __str__(self):
     return f"{self.name} on team {self.team}"
+  
