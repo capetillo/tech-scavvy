23a7afd658653290d77957a44df2af2be36cb1a7
Amy Hinds <amyhinds@Amys-MacBook-Air.local>
2019-12-20 09:07:55 -0800

    puts team model above user model to access foreign keys

--- tech_scavvy/main_app/models.py
+++ tech_scavvy/main_app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Team(models.Model):
+  team_name = models.CharField(max_length=100)
+  winner = models.BooleanField()
+  team_id = models.IntegerField()
+
+  def __str__(self):
+    return f"{self.name}"
+
 class Player(models.Model):
   name = models.CharField(max_length=100)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,19 +20,13 @@ class Player(models.Model):
   def __str__(self):
     return f"{self.name} on team {self.team}"
   
-class Team(model.Model):
-  team_name = models.CharField(max_length=100)
-  winner = models.BooleanField()
-  team_id = models.IntegerField()
 
-  def __str__(self):
-    return f"{self.name}"
 
-class Judge(model.Models):
+class Judge(models.Model):
   name = models.CharField(max_length=100)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class Task(model.Models):
+class Task(models.Model):
   task = models.CharField(max_length=100)
   team1_complete = models.BooleanField(default='False')
   team2_complete = models.BooleanField(default='False')
