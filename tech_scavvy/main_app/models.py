f34dbecf2d51abf78f13826fcce656ab198c4331
BlaineShelton <mapleteer@gmail.com>
2019-12-20 09:26:25 -0800

    adds method to reset the task after a game has been completed

--- tech_scavvy/main_app/models.py
+++ tech_scavvy/main_app/models.py
@@ -3,19 +3,21 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Judge(models.Model):
-  name = models.CharField(max_length=100)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Player(models.Model):
-  name = models.CharField(max_length=100)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  team = models.ForeignKey(Team, on_delete=models.CASCADE)
-  #designates team leader.. can only be one per team
-  leader = models.BooleanField()
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    # designates team leader.. can only be one per team
+    leader = models.BooleanField()
 
-  def __str__(self):
-    return f"{self.name} on team {self.team}"
+    def __str__(self):
+        return f"{self.name} on team {self.team}"
 
 
 class Team(models.Model):
@@ -31,16 +33,16 @@ class Task(model.Models):
     task = models.CharField(max_length=100)
     team1_complete = models.BooleanField(default='False')
     team2_complete = models.BooleanField(default='False')
-    #this allows each task that is needed in a given match to have the many to one with team 1 and team 2
+    # this allows each task that is needed in a given match to have the many to one with team 1 and team 2
     team1 = models.ForeignKey(Team, on_delete=models.CASCADE)
     team2 = models.ForeignKey(Team, on_delete=models.CASCADE)
 
-    #this allows us to know the order of the tasks and programatically work on them in that order
+    # this allows us to know the order of the tasks and programatically work on them in that order
     task_number = models.IntegerField(default=-1)
 
-
-
-
-    #TODO: add a function that resets the team1 and 2 keys and bools and task number to be reused next game
-    def reset():
-        
+    def new_game_reset(self):
+        self.team1
+        self.team1_complete = 'False'
+        self.team2
+        self.team2_complete = 'False'
+        self.task_number = -1
