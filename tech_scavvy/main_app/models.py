aedf95deb9f7c87e5e9f2a78c4ab88a90297e55a
BlaineShelton <mapleteer@gmail.com>
2019-12-20 09:51:05 -0800

    adds Match model and adds referances to all related fields as to what match they are a part of (e.g. teams and tasks)

--- tech_scavvy/main_app/models.py
+++ tech_scavvy/main_app/models.py
@@ -3,46 +3,44 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-class Judge(models.Model):
+class Match(models.Model):
     name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    winner = models.ForeignKey(Team, on_delete=models.CASCADE)
+    judge = models.ForeignKey(User,on_delete=models.CASCADE)
 
+class Team(models.Model):
+    team_name = models.CharField(max_length=100)
+    winner = models.BooleanField()
+    team_id = models.IntegerField()
+    
+    #this tells the team what match they are in
+    match = models.ForeignKey(Match)
+
+    def __str__(self):
+        return f"{self.name}"
 
 class Player(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     # designates team leader.. can only be one per team
-    leader = models.BooleanField()
+    leader = models.BooleanField(default='False')
 
     def __str__(self):
         return f"{self.name} on team {self.team}"
 
-
-class Team(models.Model):
-    team_name = models.CharField(max_length=100)
-    winner = models.BooleanField()
-    team_id = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.name}"
-
-
-class Task(model.Models):
+class Task(models.Model):
     task = models.CharField(max_length=100)
     team1_complete = models.BooleanField(default='False')
     team2_complete = models.BooleanField(default='False')
-    # this allows each task that is needed in a given match to have the many to one with team 1 and team 2
-    team1 = models.ForeignKey(Team, on_delete=models.CASCADE)
-    team2 = models.ForeignKey(Team, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match)
 
     # this allows us to know the order of the tasks and programatically work on them in that order
     task_number = models.IntegerField(default=-1)
 
     def new_game_reset(self):
-        self.team1
         self.team1_complete = 'False'
-        self.team2
         self.team2_complete = 'False'
         self.task_number = -1
+
+
