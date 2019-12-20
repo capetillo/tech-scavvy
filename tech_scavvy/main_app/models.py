301a2b9d8a265d5b8ab3c6b3d91d42e9e3386dc0
BlaineShelton <mapleteer@gmail.com>
2019-12-20 09:07:33 -0800

    add FKs to Task model for each team and a task number to give the order of the tasks to the teams

--- tech_scavvy/main_app/models.py
+++ tech_scavvy/main_app/models.py
@@ -2,32 +2,42 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Player(models.Model):
-  name = models.CharField(max_length=100)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  team = models.ForeignKey(Team, on_delete=models.CASCADE)
-  #designates team leader.. can only be one per team
-  leader = models.BooleanField()
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    # designates team leader.. can only be one per team
+    leader = models.BooleanField()
 
-  def __str__(self):
-    return f"{self.name} on team {self.team}"
-  
+    def __str__(self):
+        return f"{self.name} on team {self.team}"
+
+
 class Team(model.Model):
-  team_name = models.CharField(max_length=100)
-  winner = models.BooleanField()
-  team_id = models.IntegerField()
+    team_name = models.CharField(max_length=100)
+    winner = models.BooleanField()
+    team_id = models.IntegerField()
 
-  def __str__(self):
-    return f"{self.name}"
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Judge(model.Models):
-  name = models.CharField(max_length=100)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Task(model.Models):
-  task = models.CharField(max_length=100)
-  team1_complete = models.BooleanField(default='False')
-  team2_complete = models.BooleanField(default='False')
+    task = models.CharField(max_length=100)
+
+    #this allows each task that is needed in a given match to have the many to one with team 1 and team 2
+    team1 = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team2 = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    task_number = models.IntegerField(default=-1)
 
 
-  
+    team1_complete = models.BooleanField(default='False')
+    team2_complete = models.BooleanField(default='False')
