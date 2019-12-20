2916fa4a4760075290f46dd5480c959c58f1b4a4
Amy Hinds <amyhinds@Amys-MacBook-Air.local>
2019-12-20 09:19:33 -0800

    adds s character to model.Model, also puts Team model at top to access foreign key

--- tech_scavvy/main_app/models.py
+++ tech_scavvy/main_app/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Team(models.Model):
+    team_name = models.CharField(max_length=100)
+    winner = models.BooleanField()
+    team_id = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
+
 class Judge(models.Model):
   name = models.CharField(max_length=100)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -18,16 +26,7 @@ class Player(models.Model):
     return f"{self.name} on team {self.team}"
 
 
-class Team(models.Model):
-    team_name = models.CharField(max_length=100)
-    winner = models.BooleanField()
-    team_id = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.name}"
-
-
-class Task(model.Models):
+class Task(models.Model):
     task = models.CharField(max_length=100)
     team1_complete = models.BooleanField(default='False')
     team2_complete = models.BooleanField(default='False')
